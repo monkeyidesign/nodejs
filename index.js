058776ea7c11a5534f3025c9164d0f57353fe15b
monkeyidesign <sam@monkeyidesign.com>
2020-06-23 12:08:31 +0700

    Destructuring data

--- index.js
+++ index.js
@@ -3,46 +3,15 @@ const info = {
     name: 'Sam',
     age: new Date().getFullYear() - 1983
 };
-const copiedObject = {...info};
-console.log(copiedObject);
 
-const initArray = ['PHP', 'Laravel', 'Symfony', 'Vue', 'ReactJS', 'Flutter'];
-console.log(initArray);
-//[ 'PHP', 'Laravel', 'Symfony', 'Vue', 'ReactJS', 'Flutter' ]
-
-const copiedArray = [...initArray];
-copiedArray.push('NodeJS');
-console.log(copiedArray);
-//['PHP','Laravel', 'Symfony', 'Vue', 'ReactJS', 'Flutter', 'NodeJS']
-
-
-//push in object javascript
-let data = [];
-// ...
-data[0] = { "ID": "1", "Status": "Valid" };
-data[1] = { "ID": "2", "Status": "Invalid" };
-data[2] = { "ID": "3", "Status": "" };
-data[3] = { "ID": "4", "Status": "sgdsgsdg" };
-// ...
-let tempData = [];
-for ( let index=0; index<data.length; index++ ) {
-    //console.log(data[index].Status)
-    if ( data[index].Status === "Invalid" ) {
-        tempData.push( ...data );
-    }
+const getInfo = ({ name }) => {
+    console.log(name);
 }
-data = tempData;
-console.log(data);
+getInfo(info)
 
-// [
-//     { ID: '1', Status: 'Valid' },
-//     { ID: '2', Status: 'Invalid' },
-//     { ID: '3', Status: '' },
-//     { ID: '3', Status: 'sgdsgsdg' }
-// ]
+const {name, age } = info;
+console.log(age, name);
 
-//to merge multiple argument
-const toArray = (...args) => {
-    return args;
-};
-console.log(toArray(1,2,3,4,5))
+const serverScript = ['PHP', 'Laravel', 'Symfony'];
+const [FisrtKey, SecondKey] = serverScript;
+console.log(SecondKey);
