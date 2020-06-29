02316e186d85cffbe068292dfcb4faa6d65660f8
monkeyidesign <sam@monkeyidesign.com>
2020-06-29 10:10:37 +0700

    Sending Responses

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(res);
+    console.log(req.url, req.method, req.headers);
     res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Testing write with node</title></head><body>');
+    res.write('<div class="page">');
+    res.write('<p>Hi Node! my name is Sam adn I\'m curious to know more about you.</p>');
+    res.write('</div>');
+    res.write('</body></html>');
+    res.end();
 });
 
 server.listen(port, hostname, () => {
